92dd3b9ff5d46691c9cfab89212c95c5ace527a6
subin-tamrakar <work.subin.tamrakar@gmail.com>
2026-01-08 13:26:47 +0545

    disk usage

---
+++ task41/task41.sh
@@ -0,0 +1,8 @@
+#Write a shell script to find out disk use by /var/lib/jenkins and write the result to result.txt
+#!/usr/bin/env bash
+set -euo pipefail
+set -x
+
+du -sh /var/lib/jenkins > result.txt
+
+
